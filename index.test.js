e24e12fffd4b539527e5eaa3db4e8367438b850c
John Bogaard <bogaardj@gmail.com>
2019-10-23 06:24:49 -0500

    Create file reader files

--- index.test.js
+++ index.test.js
@@ -1,8 +1,8 @@
 describe("Tests for index js file", () => {
-  it("Red test 1", () => {
-    expect(1).toEqual(2);
+  it("Red test 1. Make sure test framework returns failure.", () => {
+    expect(3).toEqual(2);
   });
-  it("Green test 1", () => {
+  it("Green test 1. Make sure test framework returns pass.", () => {
     expect(2).toEqual(2);
   });
 });
