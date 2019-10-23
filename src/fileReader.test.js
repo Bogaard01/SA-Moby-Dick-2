63f04621a29b6e99aa15ea397dbf0e10c5efd5e7
John Bogaard <bogaardj@gmail.com>
2019-10-23 13:39:11 -0500

    used index.test, get progress. Found flaw in regex

--- src/fileReader.test.js
+++ src/fileReader.test.js
@@ -3,14 +3,22 @@ const fileReader = require("./fileReader.js");
 describe("Tests for file reader", () => {
   it("mobydick.txt read and returned as array of words.", () => {
     const testArray1 = ["Moby", "Dick"];
-    expect(fileReader.read("mobydick.txt", /\#.*|\W\s/)).toEqual(
+    expect(fileReader.read("mobydick.txt", /\#.*|\W?\s/)).toEqual(
       expect.arrayContaining(testArray1)
+    );
+  });
+  it("mobydick.txt read and not returning array of phrases.", () => {
+    const testArray1 = [
+      "public domain works in creating the Project Gutenberg-tm collection"
+    ];
+    expect(fileReader.read("mobydick.txt", /\#.*|\W?\s/)).toEqual(
+      expect.not.arrayContaining(testArray1)
     );
   });
   it("stop-words.txt read and returned as array of words.", () => {
     //console.log(fileReader.read("stop-words.txt"));
     const testArray1 = ["about", "did", "you"];
-    expect(fileReader.read("stop-words.txt", /\#.*|\W\s/)).toEqual(
+    expect(fileReader.read("stop-words.txt", /\#.*|\W?\s/)).toEqual(
       expect.arrayContaining(testArray1)
     );
   });
--- src/index.test.js
+++ src/index.test.js
@@ -7,14 +7,19 @@ describe("Tests for index js file", () => {
   //    expect(3).toEqual(2);
   //  });
   //./node_modules/.bin/jest --watchAll
-  //it("Green test 1. Make sure test framework returns pass.", () => {
-  //  expect(2).toEqual(2);
-  //});
+  it("Green test 1. Make sure test framework returns pass.", () => {
+    expect(2).toEqual(2);
+  });
+  //Tested good progress but it takes 80 seconds to run.
+  //It is 200,000 records, but that seems long time.
+  //Disabling test while I refine so Jest doesn't keep running it.
+  /*
   it("Test getting an array of counted word objects", () => {
-    var mobyArray = fileReader.read("../data/mobydick.txt", /\#.*|\W\s/);
-    var stopArray = fileReader.read("../data/stop-words.txt", /\#.*|\W\s/);
+    var mobyArray = fileReader.read("../data/mobydick.txt", /\#.*|\W?\s/);
+    var stopArray = fileReader.read("../data/stop-words.txt", /\#.*|\W?\s/);
     var resultArray = compare.removeStopWords(mobyArray, stopArray);
     var countedObj = countWords.count(resultArray);
     expect(countedObj).toEqual([{}, {}]);
   });
+  */
 });
