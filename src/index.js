e55c2d886e092364ef5bb43c44786c1d0385ea12
John Bogaard <bogaardj@gmail.com>
2019-10-23 13:23:14 -0500

    Testing progress in the index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const fileReader = require("fileReader.js");
-const compare = require("compare.js");
+const fileReader = require("./fileReader.js");
+const compare = require("./compare.js");
+const countWords = require("./countWords.js");
 
 var mobyArray = fileReader.read("../data/mobydick.txt", /\#.*|\W\s/);
 var stopArray = fileReader.read("../data/stop-words.txt", /\#.*|\W\s/);
 var resultArray = compare.removeStopWords(mobyArray, stopArray);
+var countedObj = countWords.count(resultArray);
 //console.log(resultArray);
 //console.log(resultArray.length);
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,20 @@
+const fileReader = require("./fileReader.js");
+const compare = require("./compare.js");
+const countWords = require("./countWords.js");
+
 describe("Tests for index js file", () => {
   //  it("Red test 1. Make sure test framework returns failure.", () => {
   //    expect(3).toEqual(2);
   //  });
   //./node_modules/.bin/jest --watchAll
-  it("Green test 1. Make sure test framework returns pass.", () => {
-    expect(2).toEqual(2);
+  //it("Green test 1. Make sure test framework returns pass.", () => {
+  //  expect(2).toEqual(2);
+  //});
+  it("Test getting an array of counted word objects", () => {
+    var mobyArray = fileReader.read("../data/mobydick.txt", /\#.*|\W\s/);
+    var stopArray = fileReader.read("../data/stop-words.txt", /\#.*|\W\s/);
+    var resultArray = compare.removeStopWords(mobyArray, stopArray);
+    var countedObj = countWords.count(resultArray);
+    expect(countedObj).toEqual([{}, {}]);
   });
 });
